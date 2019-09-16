f2287ce0ecae40f8046c7d4472ef0ace0460257e
Vincent Wilms <vwilms@outlook.com>
2019-09-16 18:17:27 +0200

    Create project structure.

---
+++ README.md
@@ -0,0 +1,3 @@
+# FamosFile.NET
+
+[![AppVeyor](https://ci.appveyor.com/api/projects/status/github/apollo3zehn/famosfile.net?svg=true)](https://ci.appveyor.com/project/Apollo3zehn/famosFile-net) [![NuGet](https://img.shields.io/nuget/vpre/FamosFile.NET.svg?label=Nuget)](https://www.nuget.org/packages/FamosFile.NET)
---
+++ src/FamosFile.NET/FamosFileKeyType.cs
@@ -0,0 +1,33 @@
+﻿namespace FamosFile.NET
+{
+    /// <summary>
+    /// Key type of an FamosFile key.
+    /// </summary>
+    public enum FamosFileKeyType
+    {
+        Unknown = 0,
+        CF = 1,
+        CK = 2,
+        NO = 3,
+        CB = 4,
+        CT = 5,
+        CG = 6,
+        CD = 7,
+        NT = 8,
+        CZ = 9,
+        CP = 10,
+        Cb = 11,
+        CR = 12,
+        ND = 13,
+        Cv = 14,
+        CV = 15,
+        CN = 16,
+        CS = 17,
+        NU = 18,
+        CI = 19,
+        Np = 20,
+        Ca = 21,
+        NE = 22,
+        NL = 23
+    }
+}
